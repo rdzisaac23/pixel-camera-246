a948248f8181f5d9a075496e176db81873dbafff
Erik Butcher <with_a_k@mac.com>
2020-10-28 08:43:15 -0600

    Altering comments to practice/derust merge conflict resolution.

--- app/src/main/java/com/example/pixelcamera/MainActivity.java
+++ app/src/main/java/com/example/pixelcamera/MainActivity.java
@@ -3,13 +3,14 @@ package com.example.pixelcamera;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-//The Unlimited Tacos collect 10! Sun 2 smiles.
+//The Curious Case of the Twelth Inning of the Lovers At Jazz Hands,
 public class MainActivity extends AppCompatActivity {
-    //Let's practice resolving merge conflicts.
+    //Consisting of Far More Outs Than Is Typical To The Game of Blaseball,
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    //It won't matter which line if we change them all!
+    //Due To Mysterious Avian Interference And Resulting In A Victory For The Team From San Francisco
+    //or, in short, "Crowvertime",
 }
