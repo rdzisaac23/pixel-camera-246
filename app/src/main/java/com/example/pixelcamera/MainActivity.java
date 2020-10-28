bd9026e6d80e13eb40fe27ef6f478a211bb63081
Luke Draper <72437845+LukeDraper@users.noreply.github.com>
2020-10-28 08:37:09 -0600

    Added a comment to casue merge conflict

--- app/src/main/java/com/example/pixelcamera/MainActivity.java
+++ app/src/main/java/com/example/pixelcamera/MainActivity.java
@@ -3,7 +3,7 @@ package com.example.pixelcamera;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
+//This is Luke's merge conflict.
 public class MainActivity extends AppCompatActivity {
     //This is a test comment.
     @Override
