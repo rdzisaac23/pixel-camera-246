b41cc5cc0c7af8e99ccf918d615aef036c9c568b
Luke Draper <72437845+LukeDraper@users.noreply.github.com>
2020-11-24 01:41:52 -0700

    Gave get image activity the ability to select and display an image.

--- app/src/main/java/com/example/pixelcamera/GetImageActivity.java
+++ app/src/main/java/com/example/pixelcamera/GetImageActivity.java
@@ -12,20 +12,23 @@ import android.widget.ImageView;
 
 public class GetImageActivity extends AppCompatActivity {
 
-    private static final int GALLERY_REQUEST_CODE = 529;
+    private static final int GALLERY_REQUEST_CODE = 123;
 
-    ImageView imageView = findViewById(R.id.imageView);
-    Button btn_gallery = findViewById(R.id.button_gallery);
+    ImageView imageView;
+    Button btn_gallery;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_get_image);
+        btn_gallery = findViewById(R.id.button_gallery);
+        imageView = findViewById(R.id.imageView);
 
         btn_gallery.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent();
+                intent.setType("image/^");
                 intent.setAction(Intent.ACTION_GET_CONTENT);
                 startActivityForResult(Intent.createChooser(intent, "Pick an image"), GALLERY_REQUEST_CODE);
             }
--- app/src/main/java/com/example/pixelcamera/MainActivity.java
+++ app/src/main/java/com/example/pixelcamera/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -17,6 +18,8 @@ import static java.lang.Integer.parseInt;
 import static java.lang.Math.abs;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG = "MainActivity.java";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -53,11 +56,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void btn_gallery(View view) {
+        Log.i(TAG, "Sending intent to GetImage Activity.");
         Intent intent = new Intent(this, GetImageActivity.class);
-
-
-
-
+        startActivity(intent);
     }
 
 }
