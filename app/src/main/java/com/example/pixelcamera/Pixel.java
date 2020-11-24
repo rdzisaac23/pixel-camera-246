edddfc377511d97735efa4cae8cf51ce91ffb050
Erik Butcher <with_a_k@mac.com>
2020-11-23 21:22:23 -0700

    Basic Pixel creation

--- app/src/main/java/com/example/pixelcamera/Pixel.java
+++ app/src/main/java/com/example/pixelcamera/Pixel.java
@@ -1,5 +1,19 @@
 package com.example.pixelcamera;
 
 public class Pixel {
+    private String color;
+    private boolean transparency;
 
+    Pixel(String pixelColor, boolean pixelTransparency) {
+        color = pixelColor;
+        transparency = pixelTransparency;
+    }
+
+    String getColor() {
+        return color;
+    }
+
+    boolean willDisplay() {
+        return !transparency;
+    }
 }
