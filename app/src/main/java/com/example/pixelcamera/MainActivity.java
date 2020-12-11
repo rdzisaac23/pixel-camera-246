df11a7b05098f6c4162fc844ac736fec7bab566a
Isaac Rodriguez <64040285+rdzisaac23@users.noreply.github.com>
2020-12-11 00:07:44 -0600

    Pick Color

--- app/src/main/java/com/example/pixelcamera/MainActivity.java
+++ app/src/main/java/com/example/pixelcamera/MainActivity.java
@@ -1,16 +1,62 @@
 package com.example.pixelcamera;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 //The Unlimited Tacos collect 10! Sun 2 smiles.
 //This is Luke's merge conflict.
 public class MainActivity extends AppCompatActivity {
     //Let's practice resolving merge conflicts.
+
+    ImageView mImageView;
+    View mColorView;
+    TextView mColorPicked;
+
+    Bitmap bitmap;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.pickcolor);
+
+        mImageView = findViewById(R.id.ImageView);
+        mColorPicked = findViewById(R.id.ColorPicked);
+        mColorView = findViewById(R.id.ColorView);
+
+        mImageView.setDrawingCacheEnabled(true);
+        mImageView.buildDrawingCache(true);
+
+        // image view on touch
+
+        mImageView.setOnTouchListener((v, event) -> {
+            if (event.getAction() == MotionEvent.ACTION_DOWN || event.getAction() == MotionEvent.ACTION_MOVE){
+                bitmap = mImageView.getDrawingCache();
+
+                int pixel = bitmap.getPixel((int)event.getX(), (int)event.getY());
+
+                //getting RGB values
+                int r = Color.red(pixel);
+                int g = Color.green(pixel);
+                int b = Color.blue(pixel);
+                int a = Color.alpha(pixel);
+
+                //Hex Value
+                String hex = "#"+ Integer.toHexString(pixel);
+
+                //Background Color
+                mColorView.setBackgroundColor(Color.argb(a,r,g,b));
+
+                //String Text View
+                mColorPicked.setText("RGB: "+r+", "+g+", "+b+ "\nHEX: "+hex);
+            }
+            return true;
+        });
+
     }
     //It won't matter which line if we change them all!
     //I think the User Interface on this thing will be tricky.
--- app/src/main/java/com/example/pixelcamera/ViewPixels.java
+++ app/src/main/java/com/example/pixelcamera/ViewPixels.java
@@ -1,46 +1,52 @@
-package com.example.pixelcamera;
+// I was going to change for bitMap but Luke did it awesome
+// so I stopped this and concentrate in the color.
 
-import javax.imageio.ImageIO;
-
-
-public class ViewPixels {
-    final int PIX_SIZE = 10;
-
-    // Read file
-    img =ImageIO.read(new
-
-    File("saitama_test.jpg"));
-
-    //Raster data
-    Raster src = img.getData();
-
-    // Create an identically
-    WritableRaster dest = src.createCompatibleWritableRaster();
-
-    // Loop through every PIX_SIZE
-    for(
-    int y = 0; y<src.getHeight();y +=PIX_SIZE)
-
-    {
-        for (int x = 0; x < src.getWidth(); x += PIX_SIZE) {
-
-            //Copy the pixel
-            double[] pixel = new double[3];
-            pixel = src.getPixel(x, y, pixel);
-
-            // "Paste" the pixel
-            // Never go outside
-            for (int yd = y; (yd < y + PIX_SIZE) && (yd < dest.getHeight()); yd++) {
-                for (int xd = x; (xd < x + PIX_SIZE) && (xd < dest.getWidth()); xd++) {
-                    dest.setPixel(xd, yd, pixel);
-                }
-            }
-        }
-    }
-
-    // Save the raster
-    img.setData(dest);
-
-    //Write new image
-    ImageIO.write(img, "jpg", new File('image-pixelated.jpg'));
-}
+//package com.example.pixelcamera;
+//
+//import java.awt.image.Raster;
+//import java.awt.image.WritableRaster;
+//
+//import javax.imageio.ImageIO;
+//
+//
+//public class ViewPixels {
+//    final int PIX_SIZE = 10;
+//
+//    // Read file
+//    img =ImageIO.read(new)
+//
+//    File("saitama_test.jpg"));
+//
+//    //Raster data
+//    Raster src = img.getData();
+//
+//    // Create an identically
+//    WritableRaster dest = src.createCompatibleWritableRaster();
+//
+//    // Loop through every PIX_SIZE
+//    for(
+//    int y = 0; y<src.getHeight();y +=PIX_SIZE)
+//
+//    {
+//        for (int x = 0; x < src.getWidth(); x += PIX_SIZE) {
+//
+//            //Copy the pixel
+//            double[] pixel = new double[3];
+//            pixel = src.getPixel(x, y, pixel);
+//
+//            // "Paste" the pixel
+//            // Never go outside
+//            for (int yd = y; (yd < y + PIX_SIZE) && (yd < dest.getHeight()); yd++) {
+//                for (int xd = x; (xd < x + PIX_SIZE) && (xd < dest.getWidth()); xd++) {
+//                    dest.setPixel(xd, yd, pixel);
+//                }
+//            }
+//        }
+//    }
+//
+//    // Save the raster
+//    img.setData(dest);
+//
+//    //Write new image
+//    ImageIO.write(img, "jpg", new File('image-pixelated.jpg'));
+//}
