3c533c20c2a7ef5535b93fb30de505f90035cc9b
Erik Butcher <with_a_k@mac.com>
2020-12-12 22:14:15 -0700

    Ask for permissions in GetImageActivity

--- app/src/main/java/com/example/pixelcamera/GetImageActivity.java
+++ app/src/main/java/com/example/pixelcamera/GetImageActivity.java
@@ -1,18 +1,26 @@
 package com.example.pixelcamera;
 
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 
 /**
@@ -20,12 +28,12 @@ import android.widget.ImageView;
  *
  * @author Luke Draper
  * @version 1.0
- * @date 2020-11-24
  *
  */
 public class GetImageActivity extends AppCompatActivity {
 
     private static final int GALLERY_REQUEST_CODE = 123;
+    private static final int WRITE_REQUEST_CODE = 557;
     public static final String IMAGE_URI = "com.example.pixelcamera.IMAGE_URI";
     public static final String IMAGE_PATH = "com.example.pixelcamera.IMAGE_PATH";
     private static final String TAG = "GetImageActivity.java";
@@ -49,8 +57,11 @@ public class GetImageActivity extends AppCompatActivity {
         imageView = findViewById(R.id.imageView);
 
         btn_gallery.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.M)
             @Override
             public void onClick(View v) {
+                checkPermission(Manifest.permission.READ_EXTERNAL_STORAGE, GALLERY_REQUEST_CODE);
+                checkPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE, WRITE_REQUEST_CODE);
 
                 Intent intent = new Intent();
                 intent.setType("image/^");
@@ -59,6 +70,24 @@ public class GetImageActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    /**
+     * Checks an arbitrary permission via its request code and permission name.
+     *
+     * @param permission The permission being requested.
+     * @param requestCode An app-specific integer request code corresponding to that permission.
+     */
+    public void checkPermission(String permission, int requestCode)
+    {
+
+        // Checking if permission is not granted
+        if (ContextCompat.checkSelfPermission(GetImageActivity.this, permission) == PackageManager.PERMISSION_DENIED) {
+            ActivityCompat.requestPermissions(GetImageActivity.this, new String[] { permission }, requestCode);
+        }
+        else {
+            Toast.makeText(GetImageActivity.this, "Permission already granted", Toast.LENGTH_SHORT).show();
+        }
     }
 
     /**
@@ -81,6 +110,7 @@ public class GetImageActivity extends AppCompatActivity {
             Cursor cursor = getContentResolver().query(imageData, filePath, null, null, null);
             cursor.moveToFirst();
             this.imagePath = cursor.getString(cursor.getColumnIndex(filePath[0]));
+            cursor.close();
         }
     }
 
